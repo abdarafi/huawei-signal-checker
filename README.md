4ff32d58fe03a22f1c8eae2d892292edc2271082
Abda Rafi <abda_rafi@yahoo.com>
2022-07-29 14:02:18 +0700

    init files

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# huawei-signal-checker
+# Huawei Router Signal Checker
+
+### Description
+A Python script to quickly check Huawei's router signal.
+
+### Usage
+```bash
+# Install the required libraries
+pip install -r requirements.txt
+
+# Edit the following values:
+1. YOUR_USERNAME
+2. YOUR_PASSWORD
+3. YOUR_HUAWEI_IP (Ex: 192.168.8.1 Default)
+
+# Run the script
+python signal-checker.py
+```
+
+Tested on Huawei B312, B818.
---
+++ signal-checker.py
@@ -0,0 +1,23 @@
+from huawei_lte_api.Client import Client
+from huawei_lte_api.Connection import Connection
+
+with Connection('http://YOUR_USERNAME:YOUR_PASSWORD@YOUR_HUAWEI_IP/') as connection:
+    client = Client(connection)
+
+    device_signal = client.device.signal()
+    rsrp = int(device_signal['rsrp'].strip('dBm'))
+    result = ""
+    if rsrp >= -84:
+        result = "Excellent"
+    elif rsrp <= -85 or rsrp <= -102:
+        result = "Good"
+    elif rsrp <= -103 or rsrp <= -111:
+        result = "Fair"
+    else:
+        result = "Poor"
+    
+    print('[+] Cell ID: {}'.format(device_signal['cell_id']))
+    print('[+] RSRQ: {}'.format(device_signal['rsrq']))
+    print('[+] RSRP: {}'.format(device_signal['rsrp']))
+    print('[+] RSSI: {}'.format(device_signal['rssi']))
+    print('\nOverall LTE signal strength is {}'.format(result))
